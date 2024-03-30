29353eada92fb140a9c620341dc1abc9a0eace00
admin <hoangchinhcva@gmail.com>
2024-03-30 22:45:22 +0700

    Booking component add more variable for server backend

--- src/services/userService.js
+++ src/services/userService.js
@@ -58,6 +58,9 @@ const getProfileDoctorById = (doctorId) => {
 const postPatientBookAppointment = (data) => {
     return axios.post(`/api/patient-book-appointment`, data)
 }
+const postVerifyBookAppointment = (data) => {
+    return axios.post(`/api/verify-book-appointment`, data)
+}
 
 export {
     handleLoginApi, getAllUsers,
@@ -67,5 +70,5 @@ export {
     saveDetailDoctorService, getDetailInforDoctor,
     saveBulkScheduleDoctor, getScheduleDoctorByDate,
     getExtraInforDoctorById, getProfileDoctorById,
-    postPatientBookAppointment,
+    postPatientBookAppointment, postVerifyBookAppointment,
 }
